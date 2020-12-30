d9addce16445f74398ce3d50794b4a689494dbc7
MatishaKansal <64193940+MatishaKansal@users.noreply.github.com>
2020-12-30 22:16:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import SignUpLoginScreen from "./screens/SignupLoginScreen";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <SignUpLoginScreen />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
